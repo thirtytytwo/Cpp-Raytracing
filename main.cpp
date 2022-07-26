7b105d10f1f48b87580b75444733c301b4901ba6
thirtytwo <3267207337@qq.com>
2022-07-26 17:25:53 +0800

    lesson 8.2

--- main.cpp
+++ main.cpp
@@ -6,10 +6,14 @@
 
 #include <iostream>
 
-color ray_color(const ray &r, const Hittable &world){
+color ray_color(const ray &r, const Hittable &world, int depth){
     hit_record rec;
+    if(depth <= 0){
+        return color(0,0,0);
+    }
     if(world.hit(r,0,infinity,rec)){
-        return 0.5 * (rec.normal + color(1,1,1));
+        point3 target = rec.p + rec.normal + random_in_unit_sphere();//取原点位置为p+n的单位圆上的任意点作为漫反射方向
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);//递归模拟光线漫反射
     }
     vec3 unit_dir = unit_vector(r.direction());
     auto t = 0.5 * (unit_dir.y() + 1.0);
@@ -21,7 +25,8 @@ int main(int argc, const char * argv[]){
     const double aspect_ratio = 16.0 / 9.0;
     const int width = 400;
     const int height = static_cast<int>(width / aspect_ratio);
-    const int samples_per_pixel = 100;
+    const int samples_per_pixel = 100;//规定一个像素中随机生成ray的次数
+    const int max_depth = 50;//规定光漫反射极限次数
     //WorldObject
     Hittable_list world;
     world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
@@ -38,7 +43,7 @@ int main(int argc, const char * argv[]){
                 auto u = (i + random_double()) / (width - 1);
                 auto v = (j + random_double()) / (height - 1);
                 ray r = cam.get_ray(u, v);
-                pixel_color += ray_color(r, world);
+                pixel_color += ray_color(r, world, max_depth);
             }
             write_color(std::cout, pixel_color, samples_per_pixel);
         }
